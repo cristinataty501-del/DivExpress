c3e6bab7efbef43818baca351108364e550ebe25
cristinataty501-del <cristinataty501@gmail.com>
2026-06-13 14:44:53 +0100

    Atualizacao

--- config.php
+++ config.php
@@ -1,14 +1,15 @@
 <?php
 
-$host = "dpg-d8ml628k1i2s738sggu0-a";
-$user = "divexpress_db_user";
-$pass = "4Xsaz7fLa2aafUJcs9aVfwb56qfdaxol";
-$db   = "divexpress_db";
+$host = getenv('MYSQLHOST');
+$user = getenv('MYSQLUSER');
+$pass = getenv('MYSQLPASSWORD');
+$db   = getenv('MYSQLDATABASE');
+$port = getenv('MYSQLPORT');
 
-$conn = mysqli_connect($host, $user, $pass, $db);
+$conn = mysqli_connect($host, $user, $pass, $db, $port);
 
 if (!$conn) {
-    die("Erro: " . mysqli_connect_error());
+    die("Erro de conexão: " . mysqli_connect_error());
 }
 
 ?>
